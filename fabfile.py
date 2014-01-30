db2490129ef369eb06943ed3e94941409f97d3c3
Panos Terzis <P.Terzis@ed.ac.uk>
2014-01-30 10:45:17 +0000

    fix inheritance of dictionaries

--- fabfile.py
+++ fabfile.py
@@ -676,7 +676,7 @@ def _merge(a, b, path=None):
     for key in b:
         if key in a:
             if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge(a[key], b[key], path + [str(key)])
+                _merge(a[key], b[key], path + [str(key)])
             elif a[key] == b[key]:
                 pass # same leaf value
             else:
