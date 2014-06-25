d8b6c94f8da16b326575b83066d68f5ba6e4d3cd
Murray King <murray.king@ed.ac.uk>
2014-06-25 14:10:11 +0100

    Adding aria labels and changing scale to 5

--- src/www/js/audio.js
+++ src/www/js/audio.js
@@ -35,7 +35,7 @@ define(function(){
 
     var currentAudio;
 
-    var MAX_VOLUME = 10;
+    var MAX_VOLUME = 5;
     var volume = MAX_VOLUME;
     
 /*** Audio Object decorates a Cordova Media Object ************/
@@ -168,7 +168,7 @@ return{
         return '<div class="annotate-audio-taken">' + label + '\
 <input type="hidden" value="' + url + '"/>\
 <p id="annotate-audio-position" >0.0 sec</p>\
-<span id="vol-level" class="annotate-vol-level" >10</span>\
+<span id="vol-level" class="annotate-vol-level" aria-label="Volume Level" >' + volume  +'</span>\
 <a id="annotate-audio-button" class="annotate-audio-stopped" aria-label="play audio" data-theme="a" data-iconpos="notext" href="#" data-role="button" role="button"></a>\
     <div aria-label="Volume Controls" class="volume-control-buttons" role="toolbar"> \
       <a aria-label="Increase Volume" id="increase-vol-button" data-role="button" role="button" >\
