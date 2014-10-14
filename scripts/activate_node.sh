0c016435ac6ca3c8e09b06bdf862911b4cc439e7
Ruben Gamez <ruben.gamez@ed.ac.uk>
2014-10-14 10:11:15 +0100

    Prepend the node_modules/.bin path always

--- scripts/activate_node.sh
+++ scripts/activate_node.sh
@@ -5,9 +5,5 @@
 NODE_PATH=$(pwd)/node_modules
 NODE_BIN=$(pwd)/node_modules/.bin
 
-if [ -d $NODE_PATH ]; then
-    export PATH=$NODE_BIN:$PATH
-    echo "$NODE_BIN was prepended to your PATH"
-else
-    echo "Not $NODE_PATH found"
-fi
+export PATH=$NODE_BIN:$PATH
+echo "$NODE_BIN was prepended to your PATH"
