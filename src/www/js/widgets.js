9489e57a11cbf9da1ee1bfe1b31f110d9522b839
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-02-23 17:07:49 +0000

    Add widgets.js module and create the warning widget
    As part of several issues that involve the creation
    of new widgets I'm starting to modularize the widget
    creation, validation and serialization.
    
    The warning widget is probably the most simple case
    but is an example of how to create and register custom
    widgets implementing a simple interface
    
    Issue cobweb-eu/fieldtrip-authoring-tool#18

---
+++ src/www/js/widgets.js
@@ -0,0 +1,94 @@
+define(function(require) {
+    'use strict';
+    var polygonWidget = require('widgets/polygon-widget');
+    var warningWidget = require('widgets/warning-widget');
+
+    var widgetsRegister = {};
+
+    /**
+     * Register a new widget
+     * @param widget
+     */
+    var registerWidget = function(widget) {
+        var name = widget.name;
+
+        if (!widgetsRegister.hasOwnProperty(name)) {
+            widgetsRegister[name] = [];
+        }
+        widgetsRegister[name].push(widget);
+    };
+
+    /**
+     * Get the list of widgets registered with that type
+     * @param widgetType
+     * @return Array of widgets
+     */
+    var getWidgets = function(widgetType) {
+        var widgets = [];
+        if (widgetsRegister.hasOwnProperty(widgetType)) {
+            widgets = widgetsRegister[widgetType];
+        }
+
+        return widgets;
+    };
+
+    /**
+     * Invoke the initialize function of an Array of widgets
+     * @param widgets
+     */
+    var initializeWidgets = function(widgets) {
+        var args = Array.prototype.slice.call(arguments, 1);
+
+        for (var i = 0, len = widgets.length; i < len; i++) {
+            widgets[i].initialize.apply(null, args);
+        }
+    };
+
+    /**
+     * Invoke the serialize function of an Array of widgets
+     * @param widgets
+     * @return the value of merging the calls, null if no serialize is implemented
+     */
+    var serializeWidgets = function(widgets) {
+        var args = Array.prototype.slice.call(arguments, 1);
+        var value;
+        var valueWithError;
+        var values = [];
+        var i, len;
+
+        for (i = 0, len = widgets.length; i < len; i++) {
+            valueWithError = widgets[i].serialize.apply(null, args);
+            if (valueWithError.serialize === true) {
+                values.push(valueWithError.value);
+            }
+        }
+
+        switch (values.length) {
+            case 0:
+                value = null;
+            break;
+            case 1:
+                value = values[1];
+            break;
+            default:
+                value = {};
+                for (i = 0, len = values.length; i < len; i++) {
+                    $.extend(value, values[i]);
+                }
+        }
+
+        return value;
+    };
+
+    // Register default widgets
+    registerWidget(warningWidget);
+
+    // Public interface
+    return {
+        registerWidget: registerWidget,
+        getWidgets: getWidgets,
+        initializeWidgets: initializeWidgets,
+        serializeWidgets: serializeWidgets
+    };
+
+});
---
+++ src/www/js/widgets/warning-widget.js
@@ -0,0 +1,47 @@
+define(function() {
+    'use strict';
+
+    var WIDGET_NAME = 'warning';
+
+    var addHandlers = function() {
+
+    };
+
+    var initialize = function(index, item) {
+        var $item = $(item);
+        var popup = '<div data-role="popup" class="warning-popup">' +
+                       '<h1>' + $item.find('label').text() + '</h1>' +
+                       '<div>' +
+                            '<img class="warning-icon" src="css/images/warning-icon@2x.png"/>' +
+                            '<span>' + $item.find('textarea').attr('placeholder') + '</span>' +
+                       '</div>' +
+                       '<br />' +
+                       '<a href="#" data-rel="back" data-role="button">Accept</a>' +
+                    '</div>';
+
+        $item.closest('form').append(popup);
+        $item.hide();
+    };
+
+    var validate = function(html) {
+        return {
+            valid: true,
+            errors: []
+        };
+    };
+
+    var serialize = function(html) {
+        return {
+            serialize: false,
+            value: null
+        };
+    };
+
+    return {
+        name: WIDGET_NAME,
+        initialize: initialize,
+        validate: validate,
+        serialize: serialize
+    };
+
+});
