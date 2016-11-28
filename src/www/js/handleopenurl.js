74ada82e602df16b34ee334b36600c9761831a20
Jose Lloret <jose.lloret@ed.ac.uk>
2016-11-28 15:13:24 +0000

    Custom URL schema for launching the app issue

---
+++ src/www/js/handleopenurl.js
@@ -0,0 +1,12 @@
+'use strict';
+(function(window){
+	var regex = /cityscope:\/\/.*pcapi=([^&]*)/;
+	window.handleOpenURL = function(url){
+		var result = regex.exec(url);
+		if(typeof window.pcapi.setCloudProviderUrl === 'function'){
+			if(result !== null){
+				window.pcapi.setCloudProviderUrl(result[1]);
+			}
+		}
+	};
+})(window);
